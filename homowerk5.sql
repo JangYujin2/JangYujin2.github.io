6fc862a683edd505bafa200c5a64fc0d5f97ece2
JangYujin2 <81270684+JangYujin2@users.noreply.github.com>
2021-06-16 21:45:37 +0900

    Add files via upload

---
+++ homowerk5.sql
@@ -0,0 +1,13 @@
+create database homework5;
+use homework5;
+create table sale (
+	name	char(32)	 not null primary key,
+	enter kid	    int(2),
+	enter adualt	int(2)),
+	big3 kid 	    int(2),
+	big3 adualt		int(2),
+	free kid	    int(2),
+	free adualt		int(2),
+	sum	         	int(10)
+);
+describe sale;
